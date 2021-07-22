09f3255760edddb05156a85ab605bfc0df4a70c2
cstudio7 <conquerorsword36@yahoo.com>
2021-07-22 10:22:37 -0400

    bug fix

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -13,11 +13,16 @@ module.exports.testing = {
 };
 
 module.exports.production = {
-  url: process.env.DATABASE_URL,
-  dialect: 'postgresql',
-  logging: false,
-  ssl: {
-    require: true,
-    rejectUnauthorized: false
-  }
+  database: "ddgjm91pdljcur",
+  username: "sqveaiwwweimed",
+  password: "952278fa2a8c161e2a25c2f58864190c927a501eaa5afd2a080f04492bf89845",
+  host: "ec2-108-128-104-50.eu-west-1.compute.amazonaws.com",
+  port: 5432,
+  dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  },
 };
