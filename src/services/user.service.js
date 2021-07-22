4d66fcec52c02d06823d3e6aff00248d84880b1d
cstudio7 <conquerorsword36@yahoo.com>
2021-07-22 09:21:40 -0400

    procfile added

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -45,7 +45,6 @@ class UserServices {
     try {
       const user = await db.user.findOne({
         where: {email}});
-      console.log(user)
       if (!user) return null;
       return user;
     } catch (error) {
