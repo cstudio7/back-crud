5ca74192d95bcaf34f63cb491ce567129d1149a4
cstudio7 <conquerorsword36@yahoo.com>
2021-07-22 10:31:19 -0400

    bug fix

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -13,16 +13,24 @@ module.exports.testing = {
 };
 
 module.exports.production = {
-  database: "ddgjm91pdljcur",
-  username: "sqveaiwwweimed",
-  password: "952278fa2a8c161e2a25c2f58864190c927a501eaa5afd2a080f04492bf89845",
-  host: "ec2-108-128-104-50.eu-west-1.compute.amazonaws.com",
-  port: 5432,
-  dialect: "postgres",
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false
-    }
-  },
+  url: process.env.DATABASE_URL,
+  dialect: 'postgresql',
+  logging: false,
 };
+
+
+// worked when i migrated
+// module.exports.production = {
+// database: "ddgjm91pdljcur",
+//     username: "sqveaiwwweimed",
+//     password: "952278fa2a8c161e2a25c2f58864190c927a501eaa5afd2a080f04492bf89845",
+//     host: "ec2-108-128-104-50.eu-west-1.compute.amazonaws.com",
+//     port: 5432,
+//     dialect: "postgres",
+//     dialectOptions: {
+//   ssl: {
+//     require: true,
+//         rejectUnauthorized: false
+//   }
+// },
+// };
