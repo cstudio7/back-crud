ae2b127def45e57712b7f322b30a0cfb0c6659ca
cstudio7 <conquerorsword36@yahoo.com>
2022-01-03 20:12:19 +0100

    done

--- src/database/models/chats.js
+++ src/database/models/chats.js
@@ -10,18 +10,7 @@ module.exports = (sequelize, DataTypes) => {
   );
   chat.associate = (models) => {
     // associations can be defined here
-      chat.belongsTo(
-          models.user,
-          { foreignKey: 'receiverId' },
-          { onDelete: 'cascade' },
-          { onUpdate: 'cascade' }
-      );
-      chat.belongsTo(
-          models.user,
-          { foreignKey: 'senderId' },
-          { onDelete: 'cascade' },
-          { onUpdate: 'cascade' }
-      );
+
   };
   return chat;
 };
